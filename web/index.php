195328f02a84cdc605e0fc25ceb078509539cdfc
Dylan <dylan@hypejar.com>
2013-01-16 20:30:41 -0500

    generated the styles

--- web/index.php
+++ web/index.php
@@ -1,1 +1,8 @@
+<?
+$title = 'Welcome to the McHAB Team Website';
+require('templates/start.php');
+?>
+this is the about page
 
+<?
+require('templates/end.php');
--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,5 @@
+$(function() {
+	
+	$('.image-list a').fancybox();
 
+});
