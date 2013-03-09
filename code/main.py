2b54835ac86059e9cdcbe8e190dec20dbd920260
Khoi Tran <humar104@gmail.com>
2013-03-09 22:35:16 +0000

    more

--- code/main.py
+++ code/main.py
@@ -11,8 +11,12 @@ import LSM303DLM as LSM
 import BMP085 as BMP
 import GPS
 
+class IMU_Params:
+    def __init__(self, lsm, l3g):
+
 def readIMU(arg):
     print 'IMU Read: ' + str(datetime.datetime.now())
+    accel = arg[0]
 
 def readBMP(arg):
     print 'BMP read: ' + str(datetime.datetime.now())
@@ -22,12 +26,21 @@ def readGPS(arg):
 
 
 if __name__ == '__main__':
+    #Sensor Initializations
     l3g = L3G.L3G4200D()
     l3g.enableDefault()
     lsm = LSM.LSM303DLM()
     lsm.enableDefault()
     bmp = BMP.BMP085()
     gps = GPS.GPS()
+
+    #Constants
+    NSEW_limits = [46*100+10, 45*100+25, 72*100+20, 73*100+20]
+
+    #Sampling Frequencies
+    imu_fs = 10.0
+    bmp_fs = 2.0
+    gps_fs = 1.0
 
     #Create log files
     newpath = './log'
@@ -37,9 +50,9 @@ if __name__ == '__main__':
     bmp_file = open(newpath+"/BMPData.txt","w")
     gps_file = open(newpath+"/GPSData.txt","w")
 
-    imu_task = task.LoopingCall(readIMU,[l3g,lsm]).start(0.1)
-    bmp_task = task.LoopingCall(readBMP,[bmp]).start(0.1)
-    gps_task = task.LoopingCall(readGPS,[gps]).start(1.0)
+    imu_task = task.LoopingCall(readIMU,[l3g,lsm]).start(1.0/imu_fs)
+    bmp_task = task.LoopingCall(readBMP,[bmp]).start(1.0/bmp_fs)
+    gps_task = task.LoopingCall(readGPS,[gps]).start(1.0/gps_fs)
 
     reactor.run()
 
