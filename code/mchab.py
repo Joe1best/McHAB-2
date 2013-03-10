0bd3a97a98327ceb21c8745834bfbf9c51089369
Khoi Tran <humar104@gmail.com>
2013-03-10 00:01:40 +0000

    Completed prelim rebuild

--- code/mchab.py
+++ code/mchab.py
@@ -16,34 +16,89 @@ class PersistantVars:
     accel = []
     gyro = []
     mag = []
-    
+
     alt = 0
     pressure = 0
     temp = 0
-    
-    gps_fixed = False
+
+    gps_fix = False
+
+    beep_high = False
+    beep_count = 0
+    beep_gps = False
+    beep_time = 0
+
+    mission_start = False
+    start_time = 0.0
 
     def __init__(self, lsm, l3g, bmp):
         self.lsm = lsm
         self.l3g = l3g
-        self.bmp = bmp     
+        self.bmp = bmp
 
 def readIMU(arg):
-    print 'IMU Read: ' + str(datetime.datetime.now()),
+    #print 'IMU Read: ' + str(datetime.datetime.now()),
     arg[0].accel = arg[0].lsm.readRawAccel()
     arg[0].mag = arg[0].lsm.readRawMag()
     arg[0].gyro = arg[0].l3g.readRawGyro()
     print 'accel: ' + str(arg[0].accel) + ';gyro: ' + str(arg[0].gyro) + ';mag: ' + str(arg[0].mag)
 
 def readBMP(arg):
-    print 'BMP read: ' + str(datetime.datetime.now()),
+    #print 'BMP read: ' + str(datetime.datetime.now()),
     arg[0].temp = arg[0].bmp.readTemperature()
     arg[0].pressure = arg[0].bmp.readPressure()
     arg[0].altitude = arg[0].bmp.readAltitude()
     print 'temp: ' + str(arg[0].temp) + ';pressure: ' + str(arg[0].pressure) + ';altitude: ' + str(arg[0].altitude)
 
 def readGPS(arg):
-    print 'GPS read: ' + str(datetime.datetime.now()),
+    #print 'GPS read: ' + str(datetime.datetime.now())
+    #If there's something in the serial buffer, get it
+    while(arg[1].inWaiting()>0):
+        line = arg[1].readline().rstrip().split(',')
+        if(line[0]=='$GPGGA'):
+            coord = []
+
+            if((line[6]=='1' or line[6]=='2') and arg[0].gps_fix==False):
+                coord = [float(line[2]),float(line[4])]
+                print 'We\'re locked at: ' + str(coord[0]) + ',' + str(coord[1])
+                arg[0].gps_fix=True
+
+            if(arg[0].gps_fix):
+                coord = [float(line[2]),float(line[4])]
+                if( coord[0] > NSEW[0] or coord[0] < NSEW[1] or coord[1] < NSEW[2] or coord[1] > NSEW[3] ):
+                    print 'Reached the boundary limits'
+                    arg[0].boundary_reached = True
+                if(float(line[8]) > 152.4):
+                    arg[0].mission_start = True
+                    print 'Reached 500ft, Mission Start'
+                    arg[0].start_time = time.time()*1000.0
+
+def beeper(arg):
+    if(not arg[0].gps_fix):
+        if(not arg[0].beep_high):
+            GPIO.output(25,GPIO.HIGH)
+            arg[0].beep_high = True
+            print 'High'
+        else:
+            GPIO.output(25,GPIO.LOW)
+            print 'Low'
+            arg[0].beep_time = arg[0].beep_time + 1
+            if(arg[0].beep_time > 9):
+                arg[0].beep_high = False
+                arg[0].beep_time = 0
+
+    elif(not arg[0].beep_gps and arg[0].gps_fix):
+        if(not arg[0].beep_high):
+            GPIO.output(25,GPIO.HIGH)
+            arg[0].beep_high = True
+            print 'High'
+        else:
+            GPIO.output(25,GPIO.LOW)
+            print 'Low'
+            arg[0].beep_count = arg[0].beep_count + 1
+            arg[0].beep_high = False
+            if(arg[0].beep_count > 4):
+                arg[0].beep_gps = True
 
 if __name__ == '__main__':
     #Create log files
@@ -60,11 +115,11 @@ if __name__ == '__main__':
     lsm = LSM.LSM303DLM()
     lsm.enableDefault()
     ser = serial.Serial('/dev/ttyAMA0', 4800, timeout=0.1)
-    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(25,GPIO.OUT)
+
     #Constants
     NSEW_limits = [46*100+10, 45*100+25, 72*100+20, 73*100+20]
-    start_time = time.time()*1000.0
-    mission_time = 120*60*1000.0
 
     #Sampling Frequencies
     imu_fs = 10.0
@@ -74,9 +129,10 @@ if __name__ == '__main__':
     #Object container initializations
     persistent = PersistantVars(lsm, l3g, BMP.BMP085())
 
-    imu_task = task.LoopingCall(readIMU,[persistent]).start(1.0/imu_fs)
-    bmp_task = task.LoopingCall(readBMP,[persistent]).start(1.0/bmp_fs)
+    #imu_task = task.LoopingCall(readIMU,[persistent]).start(1.0/imu_fs)
+    #bmp_task = task.LoopingCall(readBMP,[persistent]).start(1.0/bmp_fs)
     gps_task = task.LoopingCall(readGPS,[persistent, ser, NSEW_limits, start_time]).start(1.0/gps_fs)
+    beeper_task = task.LoopingCall(beeper,[persistent]).start(1.0)
 
     reactor.run()
 
