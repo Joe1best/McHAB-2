8338880016e0d7becbc7214e50306a45dc18ba05
HUmar104 <humar104@gmail.com>
2013-03-13 18:55:13 -0400

    Deleting stuff

--- code/IMU/TestAttitude.py
+++
@@ -1,7 +0,0 @@
-import Attitude
-
-att=Attitude.Attitude()
-
-while(1):
-    z,y,x=att.getAttitude()
-    print 'x: ' + str(x) + ' y: ' + str(y) + ' z: ' + str(z)
--- code/ImportDirectoryList.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-sys.path.append("./IMU")
-sys.path.append("./GPS")
-sys.path.append("./BMP085")
-
